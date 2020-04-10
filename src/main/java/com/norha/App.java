97e4e399bddc50403d2a38ed1468d2ecca794e9c
Robinclear <robin.peter.vkm@gmail.com>
2020-04-10 01:02:09 -0300

    Printed the Name Object in the console

--- src/main/java/com/norha/App.java
+++ src/main/java/com/norha/App.java
@@ -5,12 +5,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/nice")
+@Path("/app")
 public class App {
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String demo () {
+		System.out.println("Name :: "+ new Name("Robin","Peter"));
 		return "I-love-rest";
 	}
 
