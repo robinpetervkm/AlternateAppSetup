da64c8c07a5fae128cc63d94618cffcd80de959b
Robinclear <robin.peter.vkm@gmail.com>
2020-04-10 00:52:12 -0300

    new dependancy Added
    jersey jaxrs ri
    
    which solved the issue

--- src/main/java/com/norha/ApplicationConfig.java
+++ src/main/java/com/norha/ApplicationConfig.java
@@ -3,7 +3,7 @@ package com.norha;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("JAX-RS")
+@ApplicationPath("JAXRS")
 public class ApplicationConfig extends Application{
 
 }
