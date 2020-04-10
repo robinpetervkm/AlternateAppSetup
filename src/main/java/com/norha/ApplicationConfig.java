1beb2b208f6959016e932907cd66705af92a0a48
Robinclear <robin.peter.vkm@gmail.com>
2020-04-10 00:39:43 -0300

    Even After Adding the config still not getting
    JAX-RS/nice

---
+++ src/main/java/com/norha/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.norha;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("JAX-RS")
+public class ApplicationConfig extends Application{
+
+}
