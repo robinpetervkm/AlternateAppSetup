40b89ace95f8d00c9671aef546e2d7e325bc2d17
Robinclear <robin.peter.vkm@gmail.com>
2020-04-11 17:04:31 -0300

    @Singleton

--- src/main/java/com/norha/App.java
+++ src/main/java/com/norha/App.java
@@ -1,17 +1,20 @@
 package com.norha;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/app")
+@Singleton
 public class App {
 	
+	private int x = 0;
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public Name demo () {
-		return new Name("Robin","Peter");
+	public String demo () {
+		return "Value of X is "+ ++x;
 	}
 
 }
