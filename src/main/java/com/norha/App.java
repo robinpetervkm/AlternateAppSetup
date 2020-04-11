7889c19faf9cb7474ead8211788b16ae2908ec02
Robinclear <robin.peter.vkm@gmail.com>
2020-04-11 18:01:51 -0300

    BOth using function args

--- src/main/java/com/norha/App.java
+++ src/main/java/com/norha/App.java
@@ -3,19 +3,19 @@ package com.norha;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("/app")
+@Path("app/{w}")
 @Singleton
 public class App {
 	
-	private int x = 0;
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String demo (@QueryParam("x") String x) {
-		return "Value of X is "+ x;
+	public String demo (@QueryParam("x") String x, @PathParam("w") String demo) {
+		return "X = "+x+" Demo = "+demo;
 	}
 
 }
