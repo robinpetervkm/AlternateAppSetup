bb1b454401d07b8d590fcda8aec79a0e0dd8bd6e
Robinclear <robin.peter.vkm@gmail.com>
2020-04-11 21:32:58 -0300

    @Provider added

--- src/main/java/com/norha/convertors/CurrencyParamConverterProvider.java
+++ src/main/java/com/norha/convertors/CurrencyParamConverterProvider.java
@@ -5,7 +5,8 @@ import java.lang.reflect.Type;
 
 import javax.ws.rs.ext.ParamConverter;
 import javax.ws.rs.ext.ParamConverterProvider;
-
+import javax.ws.rs.ext.Provider;
+@Provider
 public class CurrencyParamConverterProvider implements ParamConverterProvider {
 
 	@Override
