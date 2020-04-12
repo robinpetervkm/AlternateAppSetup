40793f311287e6ad3e111503a4a5deb01925b6dd
Robinclear <robin.peter.vkm@gmail.com>
2020-04-11 21:07:11 -0300

    /*Param Convertors*/
    No injection source found for a parameter of type public
    java.lang.String com.norha.App.demo(com.norha.Currency) at index 0

--- src/main/java/com/norha/App.java
+++ src/main/java/com/norha/App.java
@@ -1,23 +1,19 @@
 package com.norha;
 
-import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("app/{w}")
+/*Param Convertors*/
+@Path("app")
 public class App {
-	
-	@QueryParam("x") String x;
-	@PathParam("w") String demo;
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String demo () {
-		return "X = "+x+" Demo = "+demo;
+	public String demo (@QueryParam("code") Currency currency) {
+		return "The code Currency is "+currency;
 	}
 
 }
