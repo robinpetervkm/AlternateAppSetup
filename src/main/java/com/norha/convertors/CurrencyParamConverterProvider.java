8d69ce10e6cf7c3058c98deaf55cfbf615a2c036
Robinclear <robin.peter.vkm@gmail.com>
2020-04-11 21:47:34 -0300

    Using the anonymus class to avoid crating two classes

--- src/main/java/com/norha/convertors/CurrencyParamConverterProvider.java
+++ src/main/java/com/norha/convertors/CurrencyParamConverterProvider.java
@@ -6,12 +6,31 @@ import java.lang.reflect.Type;
 import javax.ws.rs.ext.ParamConverter;
 import javax.ws.rs.ext.ParamConverterProvider;
 import javax.ws.rs.ext.Provider;
+
+import com.norha.Currency;
 @Provider
 public class CurrencyParamConverterProvider implements ParamConverterProvider {
 
 	@Override
 	public <T> ParamConverter<T> getConverter(Class<T> rawType, Type genericType, Annotation[] annotations) {
-		return (ParamConverter<T>) new CurrencyParamConverter();
+		return new ParamConverter<T> (){
+
+			@Override
+			public T fromString(String code) {
+				Currency currency=null;
+				switch(code.toUpperCase()){
+					case "EUR":currency = new Currency(code, "EURO"); break;
+					case "USD":currency = new Currency(code, "US DOLLERS"); break;
+					case "INR":currency = new Currency(code, "INDIAIN RUPESS"); break;
+				}
+				return (T) currency;
+			}
+
+			@Override
+			public String toString(T currency) {
+				return currency.toString();
+			}
+		};
 	}
 
 }
